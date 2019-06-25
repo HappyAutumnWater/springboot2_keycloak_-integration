8ad9f32d316d7b058fee3b21cf59d78210877a85
shenyanbin <Guodegang201812!>
2019-06-25 18:16:20 +0800

    1、配置logback

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -50,7 +50,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-//                .antMatchers("/phone*").hasRole("minio-role1")
+                .antMatchers("/phone*").hasRole("minio-role1")
                 .anyRequest().permitAll().and().csrf().disable().httpBasic().disable();
     }
 }
